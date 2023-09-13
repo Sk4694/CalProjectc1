473ca03bede11fd0437f9ec43720eb7f2995dc9d
Sumit <126609856+Sk4694@users.noreply.github.com>
2023-09-14 00:59:57 +0530

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -3,22 +3,33 @@ using namespace std;
 int main()
 {
     float a,b;
+    char expression;
     cout<<"Enter first number: "<< endl;
     cin>> a;
     cout<<"Enter second number: "<< endl;
     cin>> b;
-
-    char expression = '+','-','*','/';
-
+    cout<< "Enter expression " << endl;
+    cin >> expression;
+    
     switch (expression)
     {
     case '+':
-        cout<<"Sum of "<< a << "and "<< b << "are "<< a+b << endl 
+        cout<<"Sum of "<< a << " and "<< b << " are "<< a + b << endl;
         break;
-        
-    
+    case '-':
+        cout<<"Substraction of "<< a << " and "<< b << " are "<< a - b << endl;
+        break;    
+    case '*':
+        cout<<"Multiplication of "<< a << " and "<< b << " are "<< a * b << endl;
+        break; 
+    case '/':
+        cout<<"Division of "<< a << " and "<< b << " are "<< a / b << endl;
+        break;
+    case '%':
+        cout<<"Remainder of "<< a << " and "<< b << " are "<< a % b << endl; 
+        break;   
     default:
+        cout<< "Enter a valid expression" << endl;
         break;
     }
-
 }
